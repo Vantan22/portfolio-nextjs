3f97c7712680580ad66812d76c429ce2544c9fc4
vawntanng <vawntanng@gmail.com>
2025-05-24 16:41:38 +0700

    :Add Vercel Analytics and Speed Insights for performance tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
 import type React from "react"; // Import React
 import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
-
 export default function RootLayout({
   children,
 }: {
@@ -14,6 +15,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <Toaster position="top-center" />
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
